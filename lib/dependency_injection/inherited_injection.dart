96c92b282ea3c233dde4a0ead9de6e03f9775735
Siddhes Das <siddhesdas2020@gmail.com>
2026-03-11 01:49:01 +0530

    dependency injection 2

--- lib/dependency_injection/inherited_injection.dart
+++ lib/dependency_injection/inherited_injection.dart
@@ -5,19 +5,14 @@ class InheritedInjection extends InheritedWidget {
   final AppInfo _appInfo = AppInfo();
   final Widget child;
 
+  // InheritedInjection({super.key , required this.child}) : super();
   InheritedInjection({Key? key, required this.child})
       : super(key: key, child: child);
 
   AppInfo get appInfo => _appInfo;
 
-  static InheritedInjection of(BuildContext context) {
-    final inherited =
-        context.dependOnInheritedWidgetOfExactType<InheritedInjection>();
-    assert(
-      inherited != null,
-      'InheritedInjection not found in widget tree.',
-    );
-    return inherited!;
+  static InheritedInjection? of(BuildContext context) {
+    return (context.dependOnInheritedWidgetOfExactType<InheritedInjection>());
   }
 
   @override
